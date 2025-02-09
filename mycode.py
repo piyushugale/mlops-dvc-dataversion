a8aa447e22b4cd39edffb1f0acecb157da6cc8e9
piyushugale <piyushugale@gmail.com>
2025-02-09 16:58:47 +0000

    First commit before DVC

--- mycode.py
+++ mycode.py
@@ -5,7 +5,7 @@ import os
 data = {'Name': ['Alice', 'Bob', 'Charlie'],
         'Age': [25, 30, 35],
         'City': ['New York', 'Los Angeles', 'Chicago']
-    }
+       }
 
 df = pd.DataFrame(data)
 
